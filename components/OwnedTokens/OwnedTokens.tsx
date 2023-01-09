867498b4a8cb682294638a29f157d9604058a735
ruisantiago <ruisantiagomr@gmail.com>
2023-01-09 18:27:39 +0000

    Add ownedStores by ownerId method support

---
+++ components/OwnedTokens/OwnedTokens.tsx
@@ -0,0 +1,33 @@
+import { ownedStores } from "@mintbase-js/data";
+import { debounce } from "lodash";
+import { useState } from "react";
+import styles from "../../styles/Home.module.css";
+import RunButton from "../RunButton/RunButton";
+
+const TokenOwner = () => {
+  const [ownerId, setOwnerId] = useState("");
+
+  return (
+    <>
+      <div className={styles.description}>
+        <p style={{ marginBottom: "12px" }}>Enter the arguments:</p>
+        <div>
+          <input
+            className={styles.input}
+            onChange={debounce(async (e) => {
+                setOwnerId(e.target.value);
+            }, 500)}
+            placeholder="ownerId"
+          />
+        </div>
+      </div>
+
+      <RunButton
+        disabled={!ownerId}
+        method={ownedStores(ownerId)}
+      />
+    </>
+  );
+};
+
+export default TokenOwner;
